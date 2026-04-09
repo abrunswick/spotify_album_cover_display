a5c6c9b8219988876450005981555cc77b4fd930
abrunswick <anthonybrunswick4@gmail.com>
2026-04-09 01:00:37 -0400

    trying to debug goddd

--- visual.py
+++ visual.py
@@ -71,6 +71,7 @@ def update_spotify_screen(track_name, artist_name, image_filename, progress, is_
 try:
     while True:
         if os.path.exists('current_track_state.json'):
+            print("found state file")
             try:
                 with open('current_track_state.json', 'r') as f:
                     data = json.load(f)
